770730e3628aaffd671ea82c781e5e4ff27797bc
James Funk <piurafunk@gmail.com>
2018-02-19 12:18:56 -0700

    Can handle SSL now.

--- perforce/entrypoint.sh
+++ perforce/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 cleanup() {
         p4 admin stop
@@ -7,22 +8,46 @@ cleanup() {
 
 trap cleanup SIGTERM SIGHUP SIGINT
 
-p4d -d
+DAEMON_COMMAND="p4d"
+CLIENT_COMMAND="p4"
 
+# Handle the SSL set up
+if [ ! -z ${USE_SSL+x} ]; then
+	echo "Using SSL..."
+	mkdir -p $P4SSLDIR
+	chown $(whoami): $P4SSLDIR
+	chmod 500 $P4SSLDIR
+	DAEMON_COMMAND="$DAEMON_COMMAND -p ssl64:[::]:1666"
+	CLIENT_COMMAND="$CLIENT_COMMAND -p ssl:localhost:1666"
+
+	if [ ! -f "$P4SSLDIR/privatekey.txt" ] && [ ! -f "$P4SSLDIR/certificate.txt" ]; then
+		p4d -Gc
+	fi
+fi
+
+$DAEMON_COMMAND -d
+
+# Trust the server fingerprint
+if [ ! -z ${USE_SSL+x} ]; then
+	$CLIENT_COMMAND trust -y
+fi
+
+# Handle initial configuration
 if [ ! -f /opt/perforce/configured ]; then
 	# Auto-create users or not
 	if [ ! -z ${DM_USER_NOAUTOCREATE+x} ]; then
-	        p4 configure set dm.user.noautocreate=$DM_USER_NOAUTOCREATE
+	        $CLIENT_COMMAND configure set dm.user.noautocreate=$DM_USER_NOAUTOCREATE
 	fi
 
 	if [ ! -z ${SUPERUSER_USERNAME+x} ] && [ ! -z ${SUPERUSER_PASSWORD+x} ]; then
-	        p4 user -o $SUPERUSER_USERNAME | p4 user -if
-	        p4 passwd -P $SUPERUSER_PASSWORD $SUPERUSER_USERNAME
-	        echo -e "$(p4 protect -o)\n\tsuper user $SUPERUSER_USERNAME * //..." | p4 protect -i
+	        $CLIENT_COMMAND user -o $SUPERUSER_USERNAME | $CLIENT_COMMAND user -if
+	        $CLIENT_COMMAND passwd -P $SUPERUSER_PASSWORD $SUPERUSER_USERNAME
+	        echo -e "$($CLIENT_COMMAND protect -o)\n\tsuper user $SUPERUSER_USERNAME * //..." | $CLIENT_COMMAND protect -i
 	fi
 
 	touch /opt/perforce/configured
 fi
 
-tail -f /dev/null &
-wait
+$CLIENT_COMMAND admin stop
+sleep 1
+exec $DAEMON_COMMAND
